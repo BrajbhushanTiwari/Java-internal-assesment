5f979bdaa7754ded474e363f2f959979a3a4e2d5
BrajbhushanTiwari <brajbhushantiwari3008@gmail.com>
2025-06-13 18:23:10 +0530

    Add files via upload

---
+++ JavaInternalApplication.java
@@ -0,0 +1,11 @@
+package com.java_internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternalApplication.class, args);
+    }
+}
